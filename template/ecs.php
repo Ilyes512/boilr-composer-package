850d325d0b9ffc5a530c27442cf44f45c629147b
Ilyes512 <ilyes.ahidar@gmail.com>
2022-11-25 13:26:52 +0100

    Updated ecs.php config

--- template/ecs.php
+++ template/ecs.php
@@ -2,27 +2,27 @@
 
 declare(strict_types=1);
 
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff;
 use SlevomatCodingStandard\Sniffs\Classes\ClassStructureSniff;
 use SlevomatCodingStandard\Sniffs\Functions\StaticClosureSniff;
 use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 
-return static function (ECSConfig $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/vendor/ilyes512/code-style/ecs.php');
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->import(__DIR__ . '/vendor/ilyes512/code-style/ecs.php');
 
-    $containerConfigurator
-        ->parameters()
-        ->set(Option::PATHS, [
-            __DIR__ . '/src',
-            __DIR__ . '/tests',
-            __DIR__ . '/ecs.php',
-        ])
-        ->set(Option::SKIP, [
-            StaticClosureSniff::class . '.ClosureNotStatic' => [__DIR__ . '/tests'],
-            ClassStructureSniff::class . '.IncorrectGroupOrder' => [__DIR__ . '/tests'],
-            UnusedParameterSniff::class . '.UnusedParameter' => [__DIR__ . '/tests'],
-            ParameterTypeHintSniff::class . '.MissingNativeTypeHint' => [__DIR__ . '/tests/Feature/TestCase.php'],
-        ]);
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/ecs.php',
+    ]);
+
+    $ecsConfig->skip([
+        StaticClosureSniff::class . '.ClosureNotStatic' => [__DIR__ . '/tests'],
+        ClassStructureSniff::class . '.IncorrectGroupOrder' => [__DIR__ . '/tests'],
+        UnusedParameterSniff::class . '.UnusedParameter' => [__DIR__ . '/tests'],
+        ParameterTypeHintSniff::class . '.MissingNativeTypeHint' => [__DIR__ . '/tests/Feature/TestCase.php'],
+        LineLengthSniff::class . '.MaxExceeded' => [__DIR__ . '/tests'],
+    ]);
 };
